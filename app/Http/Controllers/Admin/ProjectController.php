9aa783719e3d650962f8f972e4a66048ae3f1bb2
MaTtiaa01 <mattia2001@live.it>
2023-01-11 11:20:05 +0100

    add delete

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -96,6 +96,7 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $project->delete();
+        return to_route('admin.projects.index')->with('message', "$project->title deleted successefully");
     }
 }
