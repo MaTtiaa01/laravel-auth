e38161f85ed2ba1a5cf0589e8fd92976b72764d2
MaTtiaa01 <mattia2001@live.it>
2023-01-13 16:56:53 +0100

    put img in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 
 <div class="container">
+    <div class="jumbo text-center my-5">
+        <img width="200" src="{{asset('storage/' . $project->cover_img)}}" alt="">
+    </div>
     <h1>{{$project->title}}</h1>
     <p>{{$project->description}}</p>
 </div>
